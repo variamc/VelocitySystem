bf6975426b833315e26aad6aebc9de674f0d3d2b
Kaseax <80108547+Unerheblich@users.noreply.github.com>
2022-08-20 14:51:17 +0200

    Initial Commit

---
+++ src/main/java/de/variamc/velocitysystem/VelocitySystem.java
@@ -0,0 +1,43 @@
+package de.variamc.velocitysystem;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.proxy.ProxyShutdownEvent;
+import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.proxy.ProxyServer;
+
+/**
+ * Class created by Kaseax on 2022
+ */
+@Plugin(id = "velocitysystem", name = "VelocitySystem", version = "1.0-SNAPSHOT",
+        authors = {"VariaMC, Kaseax"})
+public class VelocitySystem {
+
+    private static VelocitySystem instance;
+    private final ProxyServer server;
+
+    @Inject
+    public VelocitySystem(ProxyServer server) {
+        instance = this;
+        this.server = server;
+    }
+
+    @Subscribe
+    public void handleProxyInitialization(ProxyInitializeEvent event) {
+
+    }
+
+    @Subscribe
+    public void handleProxyShutdown(ProxyShutdownEvent event) {
+
+    }
+
+    public static VelocitySystem getInstance() {
+        return instance;
+    }
+
+    public ProxyServer getServer() {
+        return server;
+    }
+}
